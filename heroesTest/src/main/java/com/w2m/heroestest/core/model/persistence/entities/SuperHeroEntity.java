56f91667aead96edd5d2fa5762cef3554c4c3c13
javiruizidneo <106142988+javiruizidneo@users.noreply.github.com>
2024-01-26 15:18:01 +0100

    Initial commit

---
+++ heroesTest/src/main/java/com/w2m/heroestest/core/model/persistence/entities/SuperHeroEntity.java
@@ -0,0 +1,34 @@
+package com.w2m.heroestest.core.model.persistence.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.util.List;
+
+@Entity
+@Table(name = "SUPER_HERO")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class SuperHeroEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    @Column(name = "ID")
+    private long id;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @Column(name = "DESCRIPTION")
+    private String description;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "superheroId")
+    private List<HeroSuperPowerEntity> superPower;
+
+}
